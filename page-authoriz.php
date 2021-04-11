e563ed46f3c1b1cf96019accc71ffe71961cc53f
ruvick <rudikov.web@ya.ru>
2021-04-11 21:53:52 +0300

    стр. Регистрация, Авторизация, Восстановление пароля

---
+++ page-authoriz.php
@@ -0,0 +1,34 @@
+<?php get_header();
+
+/**
+ * Template Name: Шаблон страницы Авторизация
+ * Template Post Type: page
+ */
+
+ ?>
+<main class="main">	
+	<div class="bread-crumbs-box">
+		<div class="inner">	
+			<?php
+			if ( function_exists('yoast_breadcrumb') ) {
+				yoast_breadcrumb( '<div class="bread-crumbs">','</div>' );
+			}
+			?> 
+		</div>
+	</div>
+
+	<section class = "content">
+		<div class="inner">
+			<h1><?php the_title();?></h1>
+
+			<form action="#" class="authoriz__form reviews__form">
+				<input type="email" name="email" placeholder="Email" id="form-emailw" class="reviews__form-input input">
+				<input type="password" name="password" placeholder="Пароль" id="form-telw" class="reviews__form-input input">
+				<button type="submit" class="reviews__form-btn agriwind btn">Войти</button>
+			</form> 
+
+		</div>
+	</section>
+
+</main>
+<?php get_footer(); ?> 
