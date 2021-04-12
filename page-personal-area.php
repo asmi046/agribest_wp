1201c68fe4f362c274bb53927c83ee88431de5df
ruvick <rudikov.web@ya.ru>
2021-04-13 00:46:54 +0300

    Правки личного кабинета

--- page-personal-area.php
+++ page-personal-area.php
@@ -27,8 +27,8 @@
 						<h3 class="product__name">Перчатки нейлоновые без ПВХ, ХL Fiberon PSV033P (4)</h3>
 						<span class="db product__price">50 руб.</span>
 						<div class="product__bottom">
-							<a href="https://agribest.ru/agriproduct/perchatki-nejlonovye-bez-pvh-hl-fiberon-psv033p-4/" class="db btn btn__details">Подробнее</a>
-							<button class="btn btn__to-card" onclick="add_tocart(this, 0); return false;" data-price="50" data-sku="23433" data-oldprice="" data-lnk="https://agribest.ru/agriproduct/perchatki-nejlonovye-bez-pvh-hl-fiberon-psv033p-4/" data-name="Перчатки нейлоновые без ПВХ, ХL Fiberon PSV033P (4)" data-count="1" data-picture="https://agribest.ru/wp-content/uploads/2021/04/product-img-1.jpg">Повторить</button>
+							<a href="#" class="db btn btn__details">Подробнее</a>
+							<button class="btn btn__to-card">Повторить</button>
 						</div>
 					</div>
 				</div><div class="product__wr">
@@ -36,8 +36,8 @@
 						<h3 class="product__name">Бочка пластиковая пищевая квадратная c ручками, 30 л (3)</h3>
 						<span class="db product__price">1000 руб.</span>
 						<div class="product__bottom">
-							<a href="https://agribest.ru/agriproduct/bochka-plastikovaya-pishhevaya-kvadratnaya-c-ruchkami-30-l-3/" class="db btn btn__details">Подробнее</a>
-							<button class="btn btn__to-card" onclick="add_tocart(this, 0); return false;" data-price="1000" data-sku="5533" data-oldprice="" data-lnk="https://agribest.ru/agriproduct/bochka-plastikovaya-pishhevaya-kvadratnaya-c-ruchkami-30-l-3/" data-name="Бочка пластиковая пищевая квадратная c ручками, 30 л (3)" data-count="1" data-picture="https://agribest.ru/wp-content/uploads/2021/04/product-img-2.jpg">Повторить</button>
+							<a href="#" class="db btn btn__details">Подробнее</a>
+							<button class="btn btn__to-card">Повторить</button>
 						</div>
 					</div>
 				</div><div class="product__wr">
@@ -45,8 +45,8 @@
 						<h3 class="product__name">Лампа керосиновая 225, 28 см (3)</h3>
 						<span class="db product__price">568 руб.</span>
 						<div class="product__bottom">
-							<a href="https://agribest.ru/agriproduct/lampa-kerosinovaya-225-28-sm-3/" class="db btn btn__details">Подробнее</a>
-							<button class="btn btn__to-card" onclick="add_tocart(this, 0); return false;" data-price="568" data-sku="7743" data-oldprice="" data-lnk="https://agribest.ru/agriproduct/lampa-kerosinovaya-225-28-sm-3/" data-name="Лампа керосиновая 225, 28 см (3)" data-count="1" data-picture="https://agribest.ru/wp-content/uploads/2021/04/product-img-3.jpg">Повторить</button>
+							<a href="#" class="db btn btn__details">Подробнее</a>
+							<button class="btn btn__to-card">Повторить</button>
 						</div>
 					</div>
 				</div><div class="product__wr">
@@ -54,8 +54,8 @@
 						<h3 class="product__name">Перчатки нейлоновые без ПВХ, ХL Fiberon PSV033P (2)</h3>
 						<span class="db product__price">50 руб.</span>
 						<div class="product__bottom">
-							<a href="https://agribest.ru/agriproduct/perchatki-nejlonovye-bez-pvh-hl-fiberon-psv033p-2/" class="db btn btn__details">Подробнее</a>
-							<button class="btn btn__to-card" onclick="add_tocart(this, 0); return false;" data-price="50" data-sku="23433" data-oldprice="" data-lnk="https://agribest.ru/agriproduct/perchatki-nejlonovye-bez-pvh-hl-fiberon-psv033p-2/" data-name="Перчатки нейлоновые без ПВХ, ХL Fiberon PSV033P (2)" data-count="1" data-picture="https://agribest.ru/wp-content/uploads/2021/04/product-img-1.jpg">Повторить</button>
+							<a href="#" class="db btn btn__details">Подробнее</a>
+							<button class="btn btn__to-card">Повторить</button>
 						</div>
 					</div>
 				</div><div class="product__wr">
@@ -63,8 +63,8 @@
 						<h3 class="product__name">Лампа керосиновая 225, 28 см (2)</h3>
 						<span class="db product__price">568 руб.</span>
 						<div class="product__bottom">
-							<a href="https://agribest.ru/agriproduct/lampa-kerosinovaya-225-28-sm-2/" class="db btn btn__details">Подробнее</a>
-							<button class="btn btn__to-card" onclick="add_tocart(this, 0); return false;" data-price="568" data-sku="7743" data-oldprice="" data-lnk="https://agribest.ru/agriproduct/lampa-kerosinovaya-225-28-sm-2/" data-name="Лампа керосиновая 225, 28 см (2)" data-count="1" data-picture="https://agribest.ru/wp-content/uploads/2021/04/product-img-3.jpg">Повторить</button>
+							<a href="#" class="db btn btn__details">Подробнее</a>
+							<button class="btn btn__to-card">Повторить</button>
 						</div>
 					</div>
 				</div><div class="product__wr">
@@ -72,8 +72,8 @@
 						<h3 class="product__name">Бочка пластиковая пищевая квадратная c ручками, 30 л (2)</h3>
 						<span class="db product__price">1000 руб.</span>
 						<div class="product__bottom">
-							<a href="https://agribest.ru/agriproduct/bochka-plastikovaya-pishhevaya-kvadratnaya-c-ruchkami-30-l-2/" class="db btn btn__details">Подробнее</a>
-							<button class="btn btn__to-card" onclick="add_tocart(this, 0); return false;" data-price="1000" data-sku="5533" data-oldprice="" data-lnk="https://agribest.ru/agriproduct/bochka-plastikovaya-pishhevaya-kvadratnaya-c-ruchkami-30-l-2/" data-name="Бочка пластиковая пищевая квадратная c ручками, 30 л (2)" data-count="1" data-picture="https://agribest.ru/wp-content/uploads/2021/04/product-img-2.jpg">Повторить</button>
+							<a href="#" class="db btn btn__details">Подробнее</a>
+							<button class="btn btn__to-card">Повторить</button>
 						</div>
 					</div>
 				</div><div class="product__wr">
@@ -81,8 +81,8 @@
 						<h3 class="product__name">Лампа керосиновая 225, 28 см (1)</h3>
 						<span class="db product__price">568 руб.</span>
 						<div class="product__bottom">
-							<a href="https://agribest.ru/agriproduct/lampa-kerosinovaya-225-28-sm-1/" class="db btn btn__details">Подробнее</a>
-							<button class="btn btn__to-card" onclick="add_tocart(this, 0); return false;" data-price="568" data-sku="7743" data-oldprice="" data-lnk="https://agribest.ru/agriproduct/lampa-kerosinovaya-225-28-sm-1/" data-name="Лампа керосиновая 225, 28 см (1)" data-count="1" data-picture="https://agribest.ru/wp-content/uploads/2021/04/product-img-3.jpg">Повторить</button>
+							<a href="#" class="db btn btn__details">Подробнее</a>
+							<button class="btn btn__to-card">Повторить</button>
 						</div>
 					</div>
 				</div><div class="product__wr">
@@ -90,8 +90,8 @@
 						<h3 class="product__name">Перчатки нейлоновые без ПВХ, ХL Fiberon PSV033P (1)</h3>
 						<span class="db product__price">50 руб.</span>
 						<div class="product__bottom">
-							<a href="https://agribest.ru/agriproduct/perchatki-nejlonovye-bez-pvh-hl-fiberon-psv033p-1-2/" class="db btn btn__details">Подробнее</a>
-							<button class="btn btn__to-card" onclick="add_tocart(this, 0); return false;" data-price="50" data-sku="23433" data-oldprice="" data-lnk="https://agribest.ru/agriproduct/perchatki-nejlonovye-bez-pvh-hl-fiberon-psv033p-1-2/" data-name="Перчатки нейлоновые без ПВХ, ХL Fiberon PSV033P (1)" data-count="1" data-picture="https://agribest.ru/wp-content/uploads/2021/04/product-img-1.jpg">Повторить</button>
+							<a href="#" class="db btn btn__details">Подробнее</a>
+							<button class="btn btn__to-card">Повторить</button>
 						</div>
 					</div>
 				</div><div class="product__wr">
@@ -99,8 +99,8 @@
 						<h3 class="product__name">Лампа керосиновая 225, 28 см(1)</h3>
 						<span class="db product__price">568 руб.</span>
 						<div class="product__bottom">
-							<a href="https://agribest.ru/agriproduct/lampa-kerosinovaya-225-28-sm1/" class="db btn btn__details">Подробнее</a>
-							<button class="btn btn__to-card" onclick="add_tocart(this, 0); return false;" data-price="568" data-sku="7743" data-oldprice="" data-lnk="https://agribest.ru/agriproduct/lampa-kerosinovaya-225-28-sm1/" data-name="Лампа керосиновая 225, 28 см(1)" data-count="1" data-picture="https://agribest.ru/wp-content/uploads/2021/04/product-img-3.jpg">Повторить</button>
+							<a href="#" class="db btn btn__details">Подробнее</a>
+							<button class="btn btn__to-card">Повторить</button>
 						</div>
 					</div>
 				</div><div class="product__wr">
@@ -108,8 +108,8 @@
 						<h3 class="product__name">Перчатки нейлоновые без ПВХ, ХL Fiberon PSV033P (1)</h3>
 						<span class="db product__price">50 руб.</span>
 						<div class="product__bottom">
-							<a href="https://agribest.ru/agriproduct/perchatki-nejlonovye-bez-pvh-hl-fiberon-psv033p-1/" class="db btn btn__details">Подробнее</a>
-							<button class="btn btn__to-card" onclick="add_tocart(this, 0); return false;" data-price="50" data-sku="23433" data-oldprice="" data-lnk="https://agribest.ru/agriproduct/perchatki-nejlonovye-bez-pvh-hl-fiberon-psv033p-1/" data-name="Перчатки нейлоновые без ПВХ, ХL Fiberon PSV033P (1)" data-count="1" data-picture="https://agribest.ru/wp-content/uploads/2021/04/product-img-1.jpg">Повторить</button>
+							<a href="#" class="db btn btn__details">Подробнее</a>
+							<button class="btn btn__to-card">Повторить</button>
 						</div>
 					</div>
 				</div>        
