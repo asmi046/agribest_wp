aa1eddae29e3643b6f15cbc2fd8e192c20f27feb
asmi046 <asmi046@gmail.com>
2023-02-07 14:30:38 +0300

    Баннер сбербанка

--- index.php
+++ index.php
@@ -5,9 +5,17 @@
     <?php get_template_part('template-parts/banner-main-part');?>
     <?php get_template_part('template-parts/novinki-main-part');?>
     <?php get_template_part('template-parts/brand-line-part');?>
+    <section class="sb_banner">
+        <div class="inner">
+            <a href="http://www.sberbank.ru/businesscredit/partner/info?id=agribest&site=https://agribest.ru/&nocredit=true&utm_source=agribest&utm_medium=banner&utm_campaign=credit_light">
+                <img src="<?bloginfo('template_url')?>/img/rassrochka_sber_1297x369@1x.jpg" alt="Покупки в нашем магазине в кредит">
+            </a>
+        </div>
+    </section>
+    
     <?php get_template_part('template-parts/sale-main-part');?>
             
 
 </main>
 
-<?php get_footer(); ?>
+<?php get_footer(); ?>Ё
