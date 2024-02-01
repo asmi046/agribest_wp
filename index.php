df04a568893972977c0fe74bb1d243d758f0f8bd
asmi046 <asmi046@gmail.com>
2024-02-01 10:26:04 +0300

    Новая ссылка сбера +

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <?php get_template_part('template-parts/brand-line-part');?>
     <section class="sb_banner">
         <div class="inner">
-            <a href="http://www.sberbank.ru/businesscredit/partner/info?id=agribest&site=https://agribest.ru/&nocredit=true&utm_source=agribest&utm_medium=banner&utm_campaign=credit_light">
+            <a href="http://www.sberbank.ru/businesscredit/partner/info?id=agribest&site=https://agribest.ru/&nocredit=true&utm_source=agribest&utm_medium=banner&utm_campaign=credit_light&erid=2RanynLq6TS">
                 <img src="<?bloginfo('template_url')?>/img/rassrochka_sber_1297x369@1x.jpg" alt="Покупки в нашем магазине в кредит">
             </a>
         </div>
@@ -18,4 +18,4 @@
 
 </main>
 
-<?php get_footer(); ?>Ё
+<?php get_footer(); ?>
